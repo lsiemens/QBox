681e68ae05cece2d71672f5729026d6919703b3b
Luke Siemens <luke@lsiemens.com>
2020-01-04 10:58:52 -0800

    Setup prototype scripts and objects in Unity

---
+++ QBox/Assets/Scripts/MaterialController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialController : MonoBehaviour
+{
+    public Material material;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I am a material controller");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ QBox/Assets/Scripts/Scriptable Objects/WaveFunction.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// WaveFunction contains all the the nessisary information to describe a quantum
+// superposition and methods to get its time evelution.
+[CreateAssetMenu(menuName = "QBox/Quantum Objects/WaveFunction")]
+public class WaveFunction : ScriptableObject
+{
+    public QuantumSystem quantumSystem;
+}
