aed23e99c24366c6a0ba7a9f5395bced229465a0
Luke Siemens <luke@lsiemens.com>
2020-01-10 03:12:03 -0800

    Switch the number of channels to 3 and add more data to the config file.

--- QBoxPrecompute/QBoxConvert.py
+++ QBoxPrecompute/QBoxConvert.py
@@ -16,7 +16,8 @@ def _sRGBtoLinearRGB(data):
 
 class QBoxConvert:
     _EXR_ZIP = 4 # freeimage library flag: EXR_ZIP 4
-    _CHANNELS = 4 # RGBA
+    _CHANNELS = 3 # RGBA
+    _VALID_CHANNELS = [1, 3, 4] # R RGB RGBA
 
     def __init__(self, path_pk, title, path_data = ".", isLinearMode=True):
         self.path_pk = path_pk
@@ -24,39 +25,47 @@ class QBoxConvert:
         self.path_data = path_data
         self.isLinearMode = isLinearMode
         self.qboxsolver = None
+        self.config_data = {}
 
     def save(self):
         self.qboxsolver = QBox.QBox(1,1,self.path_pk) # default QBox
         self.qboxsolver.load()
 
+        self.config_data = {}
         self._save_potential()
         self._save_states()
         self._save_json()
 
     def _save_json(self):
-        data = {}
-        data["numberOfStates"] = len(self.qboxsolver.States)
-        data["energyLevels"] = self.qboxsolver.Energy_levels
-        data["isLinear"] = self.isLinearMode
+        self.config_data["numberOfStates"] = len(self.qboxsolver.States)
+        self.config_data["resolution"] = self.qboxsolver.res
+        self.config_data["xMax"] = self.qboxsolver.x_max
+        self.config_data["energyLevels"] = self.qboxsolver.Energy_levels
+        self.config_data["isLinear"] = self.isLinearMode
         with open(self.path_data + "/" + self.title + "_config.json", "w") as fout:
-            json.dump(data, fout, indent=4)
+            json.dump(self.config_data, fout, indent=4)
 
     def _save_potential(self):
         data = numpy.reshape(self.qboxsolver.V, (1,) + self.qboxsolver.V.shape)
-        data -= numpy.min(data)
-        data /= numpy.max(data)
+        data_max, data_min = numpy.max(data), numpy.min(data)
+        data = data - data_min
+        data = data/(data_max - data_min)
         data = data*2 - 1
-        self._save_EXR(data, "potential")
+        self._save_EXR(data, "potential", numChannels=1)
+
+        self.config_data["potentialMax"] = data_max
+        self.config_data["potentialMin"] = data_min
 
     def _save_states(self):
-        self._save_EXR(numpy.array(self.qboxsolver.States), "states")
+        self._save_EXR(numpy.array(self.qboxsolver.States), "states", numChannels=self._CHANNELS)
 
-    def _save_EXR(self, data, postfix):
+    def _save_EXR(self, data, postfix, numChannels=4):
+        if numChannels not in self._VALID_CHANNELS:
+            raise ValueError("Error: number of channels must be one of " + ", ".join(self._VALID_CHANNELS) + ".")
+
         image_path = self.path_data + "/" + self.title + "_" + postfix
-        data = self._pack_EXR(data)
-        print(numpy.max(data))
+        data = self._pack_EXR(data, postfix, numChannels)
         data = numpy.exp(data)
-        print(numpy.max(data))
         if not self.isLinearMode:
             data = _sRGBtoLinearRGB(data)
             image_path += "_GAMMA"
@@ -64,18 +73,25 @@ class QBoxConvert:
         data = data.astype(numpy.float32)
         imageio.imwrite(image_path + ".exr", data, format="exr", flags=self._EXR_ZIP)
 
-    def _pack_EXR(self, data):
+    def _pack_EXR(self, data, postfix, numChannels=4):
+        if numChannels not in self._VALID_CHANNELS:
+            raise ValueError("Error: number of channels must be one of " + ", ".join(self._VALID_CHANNELS) + ".")
+
         # ------------------------ IMPROVE PACKING -----------------------
-        num_images = int(numpy.ceil(len(data)/self._CHANNELS))
+        num_images = int(numpy.ceil(len(data)/numChannels))
         resolution = self.qboxsolver.res
         image_grid_dimension = int(numpy.ceil(numpy.sqrt(num_images)))
         image_resolution = image_grid_dimension*resolution
 
-        data_packed = numpy.zeros(self._CHANNELS*image_resolution**2)
+        data_packed = numpy.zeros(numChannels*image_resolution**2)
         data_packed[:len(data.ravel())] = data.ravel()
 
-        data_packed = numpy.reshape(data_packed, (image_grid_dimension, image_grid_dimension, self._CHANNELS, resolution, resolution))
+        data_packed = numpy.reshape(data_packed, (image_grid_dimension, image_grid_dimension, numChannels, resolution, resolution))
         data_packed = numpy.moveaxis(data_packed, [1, 2], [-3, -1])
-        data_packed = numpy.reshape(data_packed, (image_resolution, image_resolution, self._CHANNELS))
+        data_packed = numpy.reshape(data_packed, (image_resolution, image_resolution, numChannels))
+
+        self.config_data[postfix + "AtlasResolution"] = image_resolution
+        self.config_data[postfix + "AtlasGrid"] = image_grid_dimension
+        self.config_data[postfix + "AtlasChannels"] = numChannels
 
         return data_packed
