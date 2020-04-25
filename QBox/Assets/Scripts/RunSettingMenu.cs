6488b154f7b52ace7c26b38e8a64798e6e15d346
Luke Siemens <luke@lsiemens.com>
2020-04-25 11:11:00 -0700

    Changed info window size on android

--- QBox/Assets/Scripts/RunSettingMenu.cs
+++ QBox/Assets/Scripts/RunSettingMenu.cs
@@ -10,14 +10,19 @@ public class RunSettingMenu : MonoBehaviour
     public TextMeshProUGUI runSpeedLabel;
     public Slider runSpeed;
 
+    private bool isInitalized=false;
+
     void OnEnable() {
         runSpeedLabel.text = "Simulation\nspeed: " + runMode.speed;
         runSpeed.value = runMode.speed;
+        isInitalized = true;
     }
 
     public void OnSliderChangeValue() {
-        runMode.speed = runSpeed.value;
-        runSpeedLabel.text = "Simulation\nspeed: " + runMode.speed;
+        if (isInitalized) {
+            runMode.speed = runSpeed.value;
+            runSpeedLabel.text = "Simulation\nspeed: " + runMode.speed;
+        }
     }
 
     public void ResetSimulation() {
