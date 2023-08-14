9581a20399e5cc09dfa67b474aa003790d279cae
Luke Siemens <luke@lsiemens.com>
2023-08-13 23:20:39 -0700

    Rearranged text in the info window

--- QBox/Assets/Scripts/infoDriver.cs
+++ QBox/Assets/Scripts/infoDriver.cs
@@ -8,11 +8,10 @@ public class infoDriver : MonoBehaviour
 {
     public TextMeshProUGUI content;
     public RunMode runMode;
-    [TextArea]
-    public string text;
+    public TextAsset text;
 
     void Update() {
-        content.text = string.Format(text,
+        content.text = string.Format(text.text,
             QSystemController.currentQuantumSystem.potentialLabel,
             MaterialController.ShaderLabel(),
             QSystemController.currentQuantumSystem.length,
